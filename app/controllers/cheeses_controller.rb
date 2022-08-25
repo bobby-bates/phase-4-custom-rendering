74bdf68a2411a1ab2b9d47d44e2acb161403b288
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-25 19:13:27 -0400

    complete assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,21 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+    # render json: {
+    #   id: cheese.id,
+    #   name: cheese.name,
+    #   price: cheese.price,
+    #   is_best_seller: cheese.is_best_seller
+    # }
 
+    # REFACTOR!
+
+    cheeses = Cheese.all
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+    # render json: cheeses, only: %i[id name price is_best_seller]
+    # Or the reverse, same thing:
+    # render json: cheeses, except: %i[created_at updated_at]
+
+    render json: cheese, except: %i[created_at updated_at], methods: [:summary]
+  end
 end
